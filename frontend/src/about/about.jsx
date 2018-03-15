76f20233787e247849b75cb0b57139279d7795e7
Felipe Ramos Leandro <felipe.ramos.leandro@avanade.com>
2018-03-15 10:59:02 -0300

    Estudos react todoApp

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Fique por dentro" />
+
+        <h2>Missão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida turpis mauris, eget faucibus tellus rhoncus at. Morbi at pretium libero. Praesent facilisis ullamcorper nisi, sit amet ultrices nunc consectetur sed. Suspendisse neque metus, volutpat rutrum congue vitae, facilisis id leo. Etiam tincidunt ipsum sagittis leo varius, vitae tempor mauris tincidunt. Integer tempor blandit mi et laoreet. Nulla gravida mauris id enim lacinia, vitae imperdiet augue tincidunt.</p>
+        <p>Sed auctor neque non ullamcorper lobortis. Sed sed ullamcorper orci. Sed eleifend dictum arcu, vel efficitur lorem cursus posuere. Donec iaculis ullamcorper nulla eget bibendum. Quisque vel eros in odio sollicitudin vehicula. Phasellus elementum bibendum urna, nec varius lorem ultricies ut. Pellentesque non odio sit amet erat auctor accumsan. Sed quis enim velit. Nam rhoncus eros et tincidunt molestie. Nam imperdiet est felis, id molestie arcu auctor a.</p>
+        <h2>Visão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida turpis mauris, eget faucibus tellus rhoncus at. Morbi at pretium libero. Praesent facilisis ullamcorper nisi, sit amet ultrices nunc consectetur sed. Suspendisse neque metus, volutpat rutrum congue vitae, facilisis id leo. Etiam tincidunt ipsum sagittis leo varius, vitae tempor mauris tincidunt. Integer tempor blandit mi et laoreet. Nulla gravida mauris id enim lacinia, vitae imperdiet augue tincidunt.</p>
+        <p>Sed auctor neque non ullamcorper lobortis. Sed sed ullamcorper orci. Sed eleifend dictum arcu, vel efficitur lorem cursus posuere. Donec iaculis ullamcorper nulla eget bibendum. Quisque vel eros in odio sollicitudin vehicula. Phasellus elementum bibendum urna, nec varius lorem ultricies ut. Pellentesque non odio sit amet erat auctor accumsan. Sed quis enim velit. Nam rhoncus eros et tincidunt molestie. Nam imperdiet est felis, id molestie arcu auctor a.</p>
+        <h2>Valores</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam gravida turpis mauris, eget faucibus tellus rhoncus at. Morbi at pretium libero. Praesent facilisis ullamcorper nisi, sit amet ultrices nunc consectetur sed. Suspendisse neque metus, volutpat rutrum congue vitae, facilisis id leo. Etiam tincidunt ipsum sagittis leo varius, vitae tempor mauris tincidunt. Integer tempor blandit mi et laoreet. Nulla gravida mauris id enim lacinia, vitae imperdiet augue tincidunt.</p>
+        <p>Sed auctor neque non ullamcorper lobortis. Sed sed ullamcorper orci. Sed eleifend dictum arcu, vel efficitur lorem cursus posuere. Donec iaculis ullamcorper nulla eget bibendum. Quisque vel eros in odio sollicitudin vehicula. Phasellus elementum bibendum urna, nec varius lorem ultricies ut. Pellentesque non odio sit amet erat auctor accumsan. Sed quis enim velit. Nam rhoncus eros et tincidunt molestie. Nam imperdiet est felis, id molestie arcu auctor a.</p>
+    </div>
+)
